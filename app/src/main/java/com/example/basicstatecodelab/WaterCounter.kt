6e0e6ab503b44aa0a5be7ba83d1ab857bf9b1348
maksympash <maksympash@gmail.com>
2025-12-29 17:27:29 +0200

    Restore state in Compose

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,14 +1,13 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,35 +17,18 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
-        var count by remember  { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-
-            if (showTask){
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text(text = "You've had $count glasses.")
         }
 
-        Row () {
-            Button(
-                onClick = { count++ },
-                modifier = Modifier.padding(top = 8.dp),
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp, top = 8.dp)
-            ) {
-                Text("Clear all")
-            }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
         }
 
     }
